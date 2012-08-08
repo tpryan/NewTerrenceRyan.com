61b20e7120759ac3c126ad3489c4a852fd021c8f
Terrence Ryan <terry@terrenceryan.com>
2012-08-08 15:47:56 -0400

    Got Project caching working.

--- modules/projects.php
+++ modules/projects.php
@@ -1,27 +1,61 @@
 <?php
+include_once 'module_common.php';
 
 $count = 3;
 $github_url = "https://api.github.com/users/tpryan/repos";
-
+$github_cache = $_SERVER['DOCUMENT_ROOT'] . "/assets/cache/projects.html";
+$cache_age = 2 * 60 * 60;
 
 $github_content = get_content_from_github($github_url);
 $github_content = sort_github_results($github_content);
 
 
+if (isset($_GET['reset_cache']) && file_exists($github_cache)){
+	unlink($github_cache);
+}
 
 
-echo "<!-- pulled in from github -->" ."\n";
-for ($i=0; $i<$count; $i++){
-	echo '			<article>' . "\n";
-	echo '				<h1>';
-	echo '<a href="' . $github_content[$i]['url'] . '">';
-	echo $github_content[$i]['name'];
-	echo '</a>';
-	echo '</h1>' . "\n";
-	echo '				<p>';
-	echo $github_content[$i]['description'];
-	echo '</p>' . "\n";
-	echo '			</article>' . "\n";
+if (shouldStillBeCached($github_cache, $cache_age)){
+	$github_html = file_get_contents($github_cache);
+}
+else{
+	try {
+		$github_html = refreshGitHubHTML($github_url, $count,$github_cache);
+	} catch (Exception $e) {
+		$github_html = "<p>No upcoming events</p>";
+		var_dump($e);
+	}
+}
+
+echo $github_html;
+
+function refreshGitHubHTML($github_url, $count, $github_cache){
+	$github_content = get_content_from_github($github_url);
+	$github_html = generateGithubHTML($github_content,$count);
+	$cache_html = "<!-- From Cache --->" . $github_html;
+	file_put_contents($github_cache, $cache_html);
+	return $github_html;
+}
+
+
+function generateGithubHTML($github_content, $count){
+	$result = "";
+	$result .=  "<!-- pulled in from github -->" ."\n";
+	for ($i=0; $i<$count; $i++){
+		$item = "";
+		$item .= '			<article>' . "\n";
+		$item .= '				<h1>';
+		$item .= '<a href="' . $github_content[$i]['url'] . '">';
+		$item .= $github_content[$i]['name'];
+		$item .= '</a>';
+		$item .= '</h1>' . "\n";
+		$item .= '				<p>';
+		$item .= $github_content[$i]['description'];
+		$item .= '</p>' . "\n";
+		$item .= '			</article>' . "\n";
+		$result .= $item;
+	}
+	return $result;
 }
 
 

f4a8abd07d011faa0876d262aee2d44740422e28
Terrence Ryan <terry@terrenceryan.com>
2012-08-10 16:10:32 -0400

    Made a number of fixes to content.

--- about/index.php
+++ about/index.php
@@ -29,46 +29,46 @@
 
 <div id="content">
 	<section id="primary">		
-		<h1>About Me</h1>
-			<article>
-				<img class="picture" src="headshot2011.jpg"  width="200" height="300">
-				
-				
-				<h2>Current</h2>	
-				<p>I'm a Worldwide Developer Evangelist for <a href="http://www.adobe.com/">Adobe</a>.  
-				My job basically entails me traveling the world and 
-				talking about the developer tools and technologies that Adobe has to offer or that we support. 
-				To find out more, see about my <a href="/job">Job</a>. 
-				</p>
-				
-				<p>I'm also the author of <em>Driving Technical Change.</em> It's about convinving reluctant co-workers 
-				to adopt new tools and techniques.  To find out more, see about my <a href="/book">Book</a>. </p>
-				
-				
-				
-				<h2>Past</h2>
-				<p>I went to University of Pennsylvania, and received a Bachelors of Art in Psychology.  While I was in school, I started 
-				working in various Psychiatric clinical practices.  Gradually I shifted from psychological work to computing work. By the 
-				time I got out of school, my desire to do psychology was gone, and there were tons of tech jobs available.</p>
-				<p>I started out of school working at the Wharton School of Business. I moved from tech support to server admin to 
-				system programmer to developer.  After 10 years there, I had done just about every technical job there is to do, and 
-				decided programming was where I belonged. </p>
-				
-				
-				<h2>Site</h2>
-				<h3>Front End</h3>
-				<p>This is my second attempt at building a semantic HTML5 site.</p>
-				<p>I use Typekit to render <a href="https://typekit.com/fonts/myriad-pro-condensed">Myriad Pro Condensed</a> 
-				as a the header font and <a href="https://typekit.com/fonts/adobe-caslon-pro">Adobe Caslon Pro</a> for the copy font. </p>
-			 
-				
-				
-				<h3>Tools</h3>
-				<p>I used <a href="http://browserlab.adobe.com">Browser Lab</a> to get the site working cross platform/browser.</p>	
-				<p>The site was composed using <a href="http://www.adobe.com/products/fireworks/">Fireworks CS5</a>.</p>
-				<p>The code was written using <a href="http://www.sublimetext.com/2">Sublime Text 2</a> 
-					and <a href="http://www.brackets.io">Brackets</a>.</p>
-				<p>I also used <a href="http://sass-lang.com/">Sass</a> to speed up my CSS work.</p>	
+		<article>
+			<h1>About Me</h1>
+			<img class="picture" src="headshot2011.jpg"  width="200" height="300">
+			
+			
+			<h2>Current</h2>	
+			<p>I'm a Worldwide Developer Evangelist for <a href="http://www.adobe.com/">Adobe</a>.  
+			My job basically entails me traveling the world and 
+			talking about the developer tools and technologies that Adobe has to offer or that we support. 
+			To find out more, see about my <a href="/job">Job</a>. 
+			</p>
+			
+			<p>I'm also the author of <em>Driving Technical Change.</em> It's about convinving reluctant co-workers 
+			to adopt new tools and techniques.  To find out more, see about my <a href="/book">Book</a>. </p>
+			
+			
+			
+			<h2>Past</h2>
+			<p>I went to University of Pennsylvania, and received a Bachelors of Art in Psychology.  While I was in school, I started 
+			working in various Psychiatric clinical practices.  Gradually I shifted from psychological work to computing work. By the 
+			time I got out of school, my desire to do psychology was gone, and there were tons of tech jobs available.</p>
+			<p>I started out of school working at the Wharton School of Business. I moved from tech support to server admin to 
+			system programmer to developer.  After 10 years there, I had done just about every technical job there is to do, and 
+			decided programming was where I belonged. </p>
+			
+			
+			<h2>Site</h2>
+			<h3>Front End</h3>
+			<p>This is my second attempt at building a semantic HTML5 site.</p>
+			<p>I use Typekit to render <a href="https://typekit.com/fonts/myriad-pro-condensed">Myriad Pro Condensed</a> 
+			as a the header font and <a href="https://typekit.com/fonts/adobe-caslon-pro">Adobe Caslon Pro</a> for the copy font. </p>
+		 
+			
+			
+			<h3>Tools</h3>
+			<p>I used <a href="http://browserlab.adobe.com">Browser Lab</a> to get the site working cross platform/browser.</p>	
+			<p>The site was composed using <a href="http://www.adobe.com/products/fireworks/">Fireworks CS5</a>.</p>
+			<p>The code was written using <a href="http://www.sublimetext.com/2">Sublime Text 2</a> 
+				and <a href="http://www.brackets.io">Brackets</a>.</p>
+			<p>I also used <a href="http://sass-lang.com/">Sass</a> to speed up my CSS work.</p>	
  
 		</article>		
 	</section>				
---
+++ job/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Job - TerrenceRyan.com</title>
+	<link rel="stylesheet" href="/assets/css/main.css" type="text/css" media="screen" />
+	<script type="text/javascript" src="http://use.typekit.com/puk0eup.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<body id="job">
+<header>
+	<div id="masthead">
+		<hgroup>
+			<h1><a href="/">TerrenceRyan.com</a></h1>
+			<p>I'm a 35 year old redhead geek from Philly. <br /> 
+				<span class="hidesmall">I'm an Evangelist for <a href="/job">Adobe</a>.<br />
+				Also wrote <a href="/book">Driving Technical Change</a></span>
+			</p>
+		</hgroup>	
+		<nav>
+			<ul>
+				<li><a href="/about">About Me</a></li>
+				<li><a href="/book">My Book</a></li>
+			</ul>
+		</nav>
+	</div>	
+	
+</header>   
+
+<div id="content">
+	<section id="primary">
+		<article>		
+		<h1>Job</h1>
+			<img class="image" src="adobelogo.png" width="283" height="355" id="adobelogo">
+			
+			<p>I work as a Worldwide Developer Evangelist for Adobe Systems. My job basically entails me traveling the world and 
+			talking about the developer tools and technologies that Adobe has to offer or that we support.</p> 
+			
+			<p>Before we go any futher, I will make the following disclaimer:</p>
+			<div id="disclaimerpad">
+			<blockquote id="disclaimer"><p>This is a personal blog. While Adobe paying my salary 
+			might not make me completely objective, the opinions 
+			expressed here are mine and not theirs.</p> </blockquote>
+			</div>
+			
+			<p>My major goal is to help developers using Adobe technologies to be successful. Hopefully they'll be using 
+			Adobe tools to do that, but it's important to keep in mind that sometimes they can't.</p>
+ 
+		</article>		
+	</section>				
+</div>
+<footer>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/sitemap">Sitemap</a></li>      
+			<li><a href="/search">Search</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
+	<small id="copyright">Copyright 2008 - 2012 Terrence Ryan</small>
+	<small id="disclaimer">This is a personal blog. While Adobe paying my salary might 
+	not make me completely  objective, the opinions expressed here are mine and not theirs.</small>
+</footer>
+</body>
+</html>

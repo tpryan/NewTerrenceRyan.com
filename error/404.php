edd177b479b2d2e6748d55c4e20645671a83ecc5
Terrence Ryan <terry@terrenceryan.com>
2012-08-27 11:40:58 -0400

    Getting 404 working.

---
+++ error/404.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<title>404 - TerrenceRyan.com</title>
+	<link rel="stylesheet" href="/assets/css/main.css" type="text/css" media="screen" />
+	<script type="text/javascript" src="http://use.typekit.com/puk0eup.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<body id="error">
+<header>
+	<div id="masthead">
+		<hgroup>
+			<h1><a href="/">TerrenceRyan.com</a></h1>
+			<p>I'm a 35 year old redhead geek from Philly. <br /> 
+				<span class="hidesmall">I'm an Evangelist for <a href="/job">Adobe</a>.<br />
+				Also wrote <a href="/book">Driving Technical Change</a></span>
+			</p>
+		</hgroup>	
+		<nav>
+			<ul>
+				<li><a href="/about">About Me</a></li>
+				<li><a href="/book">My Book</a></li>
+			</ul>
+		</nav>
+	</div>	
+	
+</header>   
+
+<div id="content">
+	<section id="primary">		
+		<article>
+			<h1>404</h1>
+			<img src="/error/hipster.jpg" width="200" height="257" class="image" />
+			<p>I know, you used to go to this page <em>all the time</em> back in the day. Then it got trendy, and over crowded
+				with lame people who listen to Coldplay.  And now it's gone.  Gone!  All because you couldn't keep it a secret. 
+				You have only yourself to blame.</p>
+			<p>On a more helpful note, perhaps you should try a <a href="/search">search</a>, or start over again at the <a href="/">main page</a>.</p>
+ 
+		</article>		
+	</section>				
+</div>
+<footer>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/sitemap">Sitemap</a></li>      
+			<li><a href="/search">Search</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
+	<small id="copyright">Copyright 2008 - 2012 Terrence Ryan</small>
+	<small id="disclaimer">This is a personal blog. While Adobe paying my salary might 
+	not make me completely  objective, the opinions expressed here are mine and not theirs.</small>
+</footer>
+</body>
+</html>

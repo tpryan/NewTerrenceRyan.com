82bf2d6567c549db442d6e3451f8b7aea00e41c2
Terrence Ryan <terry@terrenceryan.com>
2012-08-27 11:41:11 -0400

    Getting blog redirecting working.

--- blog/index.php
+++ blog/index.php
@@ -1,1 +1,13 @@
-yo!
+<?php
+
+$redirect = false;
+if (stripos($_SERVER["REQUEST_URI"], "/blog/post.cfm") > -1){
+	$url_part = str_replace("/blog/post.cfm", "", $_SERVER["REQUEST_URI"]);
+	$url = "http://blog.terrenceryan.dev" . $url_part;
+} else {
+	$url = "http://new.terrenceryan.dev/error/404.php";
+}
+
+header("Location: " . $url);
+
+?>

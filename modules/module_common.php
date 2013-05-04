8383ef14ff2643234938d5e115bd84334e7fb3ae
Terrence Ryan <terry@terrenceryan.com>
2013-05-04 19:09:50 -0400

    Fixed Github calls.

--- modules/module_common.php
+++ modules/module_common.php
@@ -15,4 +15,18 @@
 		}
 	}
 
+	function url_get_contents ($Url) {
+	    if (!function_exists('curl_init')){ 
+	        die('CURL is not installed!');
+	    }
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL, $Url);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch,CURLOPT_USERAGENT,'PHP_Curl');
+	    $output = curl_exec($ch);
+	    curl_close($ch);
+
+	    return $output;
+	}
+
 ?>
--- modules/projects.php
+++ modules/projects.php
@@ -2,12 +2,10 @@
 include_once 'module_common.php';
 
 $count = 3;
-$github_url = "https://api.github.com/users/tpryan/repos";
+$github_url = "https://api.github.com/users/tpryan/repos?sort=pushed";
 $github_cache = $_SERVER['DOCUMENT_ROOT'] . "/assets/cache/projects.html";
 $cache_age = 2 * 60 * 60;
 
-//$github_content = get_content_from_github($github_url);
-//$github_content = sort_github_results($github_content);
 
 
 if (isset($_GET['reset_cache']) && file_exists($github_cache)){
@@ -23,7 +21,7 @@ else{
 		$github_html = refreshGitHubHTML($github_url, $count,$github_cache);
 	} catch (Exception $e) {
 		$github_html = "<article><p>No current projects</p></article>";
-		//var_dump($e);
+		var_dump($e);
 	}
 }
 
@@ -31,19 +29,18 @@ echo $github_html;
 
 function refreshGitHubHTML($github_url, $count, $github_cache){
 	$github_content = get_content_from_github($github_url);
-	$github_content = sort_github_results($github_content);
 	$github_html = generateGithubHTML($github_content,$count);
 	$cache_html = "<!-- From Cache --->" . $github_html;
 	file_put_contents($github_cache, $cache_html);
 	return $github_html;
 }
 
-
 function generateGithubHTML($github_content, $count){
 	$result = "";
 	$result .=  "<!-- pulled in from github -->" ."\n";
 	for ($i=0; $i<$count; $i++){
 		$item = "";
+		$item .= "<!-- pushed_at" . $github_content[$i]['pushed_at'] . " -->" ."\n";
 		$item .= '			<article>' . "\n";
 		$item .= '				<h1>';
 		$item .= '<a href="' . $github_content[$i]['html_url'] . '">';
@@ -60,19 +57,8 @@ function generateGithubHTML($github_content, $count){
 }
 
 
-function sort_github_results($github_content){
-	$dates = array();
-	foreach ($github_content as $key => $row)
-	{
-	    $dates[$key] = $row['pushed_at'];
-	}
-	array_multisort($dates, SORT_DESC, $github_content);
-	return $github_content;
-}
-
-
 function get_content_from_github($url) {
-	$content = file_get_contents($url);
+	$content = url_get_contents($url);
 	return json_decode($content, true);
 }
 

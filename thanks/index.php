176045dcb858e65ee1d5bbb1e027f3bdea6ab6dc
Terrence Ryan <terry@terrenceryan.com>
2013-09-19 11:25:57 -0400

    Checking in Thank you page.

---
+++ thanks/index.php
@@ -0,0 +1,47 @@
+<?php $page_title="Thanks - TerrenceRyan.com";  include '../modules/head.php';?>
+<?php include '../modules/page-header.php';?>  
+
+
+<div class="top-bar">
+	<div class="page-title">
+		<h1>Thank You</h1>
+		<?php include '../modules/nav.php';?>
+	</div>	
+</div>
+
+<div class="main thanks">
+	<article>
+		<div class="column full">
+			<h1>University of the Arts in Philadelphia</h1>
+			<time>September 19, 2013</time>
+			<h2>Creative Cloud</h2>
+			<ul>
+				<li><a href="http://www.adobe.com/products/creativecloud.html">Creative Cloud Overview</a></li>
+			</ul>
+			<h2>New Features that you might like</h2>
+			<ul>
+				<li><a href="http://blogs.adobe.com/photoshopdotcom/2013/09/introducing-adobe-generator-for-photoshop-cc.html">Photoshop Generator</a></li>
+				<li><a href="http://blogs.adobe.com/edgereflow/2013/09/09/photoshop-reflow/">Photoshop &amp; Reflow</a></li>
+				<li><a href="http://www.adobe.com/products/photoshop/features.html">Photoshop Features</a></li>
+				<li><a href="http://www.adobe.com/products/illustrator/features.html">Illustrator Features</a></li>
+				<li><a href="http://typekit.com">Typekit</a></li>
+			</ul>
+			<h2>Inspiration</h2>
+			<ul>
+				<li><a href="http://petapixel.com/2013/07/20/redditor-shows-off-his-restoration-and-colorization-skills-via-time-lapse/">Redditor shows off his restoration and colorization skills via time lapse</a></li>
+				<li><a href="http://typedia.com/blog/post/behind-the-typedia-logo-design/">Behnind the Typedia Logo Design</a></li>
+				<li><a href="http://erikjohanssonphoto.com/">Erik Johansson</a></li>
+				<li><a href="http://www.alex-buono.com/">Alex Buono</a></li>	
+			</ul>
+			<h2>Learning Adobe Tools</h2>
+			<ul>
+				<li><a href="https://helpx.adobe.com/creative-cloud/products.html">Adobe Learning Content</a></li>
+				<li><a href="http://tv.adobe.com/">Adobe TV</a></li>
+			</ul>	
+		</div>	
+	</article>
+</div>
+<?php include '../modules/page-footer.php';?>
+<?php include '../modules/google.php';?>
+</body>
+</html>

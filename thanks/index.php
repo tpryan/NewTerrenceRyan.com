9596f6272a356cc1c201997407f1c1ff0b6d5ccd
Terrence Ryan <terry@terrenceryan.com>
2013-10-22 15:59:21 -0400

    Updating thanks page.

--- thanks/index.php
+++ thanks/index.php
@@ -10,37 +10,47 @@
 </div>
 
 <div class="main thanks">
-	<article>
+    <p>Thanks for coming out to see me today! I hope my talk was helpful for you. </p>
+    
+    
+    <article>
 		<div class="column full">
-			<h1>University of the Arts in Philadelphia</h1>
-			<time>September 19, 2013</time>
-			<h2>Creative Cloud</h2>
-			<ul>
-				<li><a href="http://www.adobe.com/products/creativecloud.html">Creative Cloud Overview</a></li>
-			</ul>
-			<h2>New Features that you might like</h2>
-			<ul>
-				<li><a href="http://blogs.adobe.com/photoshopdotcom/2013/09/introducing-adobe-generator-for-photoshop-cc.html">Photoshop Generator</a></li>
-				<li><a href="http://blogs.adobe.com/edgereflow/2013/09/09/photoshop-reflow/">Photoshop &amp; Reflow</a></li>
-				<li><a href="http://www.adobe.com/products/photoshop/features.html">Photoshop Features</a></li>
-				<li><a href="http://www.adobe.com/products/illustrator/features.html">Illustrator Features</a></li>
-				<li><a href="http://typekit.com">Typekit</a></li>
-				<li><a href="http://behance.com">Behance</a></li>
-			</ul>
-			<h2>Inspiration</h2>
-			<ul>
-				<li><a href="http://petapixel.com/2013/07/20/redditor-shows-off-his-restoration-and-colorization-skills-via-time-lapse/">Redditor shows off his restoration and colorization skills via time lapse</a></li>
-				<li><a href="http://typedia.com/blog/post/behind-the-typedia-logo-design/">Behnind the Typedia Logo Design</a></li>
-				<li><a href="http://erikjohanssonphoto.com/">Erik Johansson</a></li>
-				<li><a href="http://www.alex-buono.com/">Alex Buono</a></li>	
-			</ul>
-			<h2>Learning Adobe Tools</h2>
-			<ul>
-				<li><a href="https://helpx.adobe.com/creative-cloud/products.html">Adobe Learning Content</a></li>
-				<li><a href="http://tv.adobe.com/">Adobe TV</a></li>
-			</ul>	
-		</div>	
-	</article>
+			<h1>HTML5 Developers Conference</h1>
+			<time>October 22, 2013</time>
+        </div>
+        <h2>Programming CSS</h2>
+        <h3>Source</h3>
+        <ul>
+            <li><a href="https://github.com/tpryan/Max2013---Programming-CSS">Programming-CSS</a></li>
+        </ul>
+        <h3>Video</h3>
+        <ul>
+            <li><a href="http://tv.adobe.com/watch/max-2013/programming-in-css/">Previous Recording </a></li>
+        </ul>
+        
+        <h3>References</h3>
+        <h4>General</h4>
+        <ul>
+            <li><a href="http://csswizardry.com/2012/11/code-smells-in-css/">Code smells in CSS</a></li>
+            <li><a href="http://coding.smashingmagazine.com/2007/07/27/css-specificity-things-you-should-know/">CSS Specificity: Things You Should Know</a></li>
+            <li><a href="http://incident57.com/codekit/">Codekit</a></li>
+            <li><a href="http://www.stuffandnonsense.co.uk/archives/css_specificity_wars.html">CSS: Specificity Wars</a></li>
+        </ul>
+        <h4>Objected Oriented CSS</h4>
+        <ul>
+            <li><a href="http://coding.smashingmagazine.com/2011/12/12/an-introduction-to-object-oriented-css-oocss/">An Introduction To Object Oriented CSS (OOCSS)</a></li>
+            <li><a href="https://github.com/stubbornella/oocss/wiki">Object Oriented CSS</a></li>
+        </ul>
+        <h4>Preprocessors</h4>
+        <ul>
+            <li><a href="http://net.tutsplus.com/tutorials/html-css-techniques/sass-vs-less-vs-stylus-a-preprocessor-shootout/">Sass vs. LESS vs. Stylus: Preprocessor Shootout</a></li>
+            <li><a href="https://gist.github.com/paulmillr/2005644">paulmillr / less.less</a></li>
+            <li><a href="http://www.scottlogic.com/blog/2013/03/08/less-vs-sass-vs-stylus.html">LESS VS SASS VS STYLUS</a></li>
+            <li><a href="http://css-tricks.com/sass-vs-less/">Sass vs. LESS</a></li>
+            <li><a href="https://coderwall.com/p/p7suww">Stylus is better than LESS and SASS</a></li>
+        </ul>
+    </article>        
+	
 </div>
 <?php include '../modules/page-footer.php';?>
 <?php include '../modules/google.php';?>

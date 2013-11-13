994d6f82412ade4cd4cecb77223f98f0520583c5
Terrence Ryan <terry@terrenceryan.com>
2013-11-13 14:15:03 -0500

    Fixxing issue with social network display on smaller devices.

--- about/index.php
+++ about/index.php
@@ -38,45 +38,40 @@
 		
 	 	<div class="column half">
 	 		<h2>Social Networks</h2>
-			<table id="socialnetworks">
-				<tr>
-					<td>
-						<a href="http://www.facebook.com/terry.ryan" title="Facebook | Terrence Ryan">
-							<img src="/assets/img/facebookicon.png" height="55" width="116" alt="Facebook" />
-						</a>
-					</td>
-				
-					<td>
-						<a href="http://twitter.com/tpryan" title="Twitter / tpryan">
-							<img src="/assets/img/twitter.png" height="55" width="155" alt="Twitter" class="twitter" />
-						</a>
-					</td>
-					<td>
-						<a href="http://twitter.com/tpryan" title="Lanyrd / tpryan">
-							<img src="/assets/img/lanyrd.png" height="36" width="150" alt="Lanyrd" class="lanyrd" />
-						</a>
-					</td>
-				</tr>
-				
-				<tr>
-					<td>
-						<a href="http://www.linkedin.com/in/terrencepryan" title="LinkedIn: Terrence Ryan">
-						<img src="/assets/img/linkedin-icon.png" height="35" width="121" alt="LinkedIn" />
-					</a>	
-					</td>
-					<td>
-						<a href="https://github.com/tpryan" title="github: Terrence Ryan">
-						<img src="/assets/img/githublogo.png" height="45" width="100" alt="github" />
-					</a>	
-					</td>
-					<td>
-						<a href="http://www.behance.net/tpryan" title="Behance: Terrence Ryan">
-						<img src="/assets/img/behance.png" height="56" width="150" alt="behance" />
-					</a>	
-					</td>
-				</tr>
+			<ul id="socialnetworks">
+				<li>
+                    <a href="http://www.facebook.com/terry.ryan" title="Facebook | Terrence Ryan">
+                        <img src="/assets/img/facebookicon.png" height="55" width="116" alt="Facebook" />
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="http://twitter.com/tpryan" title="Twitter / tpryan">
+                        <img src="/assets/img/twitter.png" height="55" width="155" alt="Twitter" class="twitter" />
+                    </a>
+                </li>
+                <li>
+                    <a href="http://twitter.com/tpryan" title="Lanyrd / tpryan">
+                        <img src="/assets/img/lanyrd.png" height="36" width="150" alt="Lanyrd" class="lanyrd" />
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.linkedin.com/in/terrencepryan" title="LinkedIn: Terrence Ryan">
+                    <img src="/assets/img/linkedin-icon.png" height="35" width="121" alt="LinkedIn" />
+                </a>	
+                </li>
+                <li>
+                    <a href="https://github.com/tpryan" title="github: Terrence Ryan">
+                    <img src="/assets/img/githublogo.png" height="45" width="100" alt="github" />
+                </a>	
+                </li>
+                <li>
+                    <a href="http://www.behance.net/tpryan" title="Behance: Terrence Ryan">
+                    <img src="/assets/img/behance.png" height="56" width="150" alt="behance" />
+                </a>	
+                </li>
 			
-			</table>
+			</ul>
 	 		<h2>Drop me an email</h2>
 			<p id="response">Use form to send email. </p>
 			<form name="contact_form" id="contact_form" action="process.php" method="post" >

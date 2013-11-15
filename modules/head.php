18f65909c09cc4b5407ec68139e39602d6cb3b9b
Terrence Ryan <terry@terrenceryan.com>
2013-11-14 21:30:57 -0500

    fixing font issues.

--- modules/head.php
+++ modules/head.php
@@ -14,11 +14,11 @@ if (!isset($page_title)) {
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width" />
 	<title><?php echo $page_title;?></title>
+    <?php include 'typekit.php';?>  
 	<link rel="alternate" type="application/rss+xml" title="RSS Feeds" href="http://feeds.feedburner.com/Terrenceryan" />
 	<link rel="stylesheet" href="/assets/css/2013.css" type="text/css" media="screen" />
 	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-	<script type="text/javascript" src="//use.typekit.net/rbx8rox.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	
 	<?php if ($include_contact) {
 	echo '<script type="text/javascript" src="/assets/js/jquery-1.8.0.min.js"></script>' , chr(10);
 	echo chr(9),'<script type="text/javascript" src="/assets/js/contact.js"></script>';

f5803b4c42b95c5e7930435e6df0ac1506b33a5d
Terrence Ryan <terry@terrenceryan.com>
2014-08-22 09:51:25 -0400

    Tweaking content.

--- about/index.php
+++ about/index.php
@@ -36,73 +36,7 @@
 		</div>
 		
 		
-	 	<div class="column half">
-	 		<h2>Social Networks</h2>
-			<ul id="socialnetworks">
-				<li>
-                    <a href="http://www.facebook.com/terry.ryan" title="Facebook | Terrence Ryan">
-                        <img src="/assets/img/facebookicon.png" height="55" width="116" alt="Facebook" />
-                    </a>
-                </li>
-            
-                <li>
-                    <a href="http://twitter.com/tpryan" title="Twitter / tpryan">
-                        <img src="/assets/img/twitter.png" height="55" width="155" alt="Twitter" class="twitter" />
-                    </a>
-                </li>
-                <li>
-                    <a href="http://twitter.com/tpryan" title="Lanyrd / tpryan">
-                        <img src="/assets/img/lanyrd.png" height="36" width="150" alt="Lanyrd" class="lanyrd" />
-                    </a>
-                </li>
-                <li>
-                    <a href="http://www.linkedin.com/in/terrencepryan" title="LinkedIn: Terrence Ryan">
-                    <img src="/assets/img/linkedin-icon.png" height="35" width="121" alt="LinkedIn" />
-                </a>	
-                </li>
-                <li>
-                    <a href="https://github.com/tpryan" title="github: Terrence Ryan">
-                    <img src="/assets/img/githublogo.png" height="45" width="100" alt="github" />
-                </a>	
-                </li>
-                <li>
-                    <a href="http://www.behance.net/tpryan" title="Behance: Terrence Ryan">
-                    <img src="/assets/img/behance.png" height="56" width="150" alt="behance" />
-                </a>	
-                </li>
-			
-			</ul>
-	 		<h2>Drop me an email</h2>
-			<p id="response">Use form to send email. </p>
-			<form name="contact_form" id="contact_form" action="process.php" method="post" >
-				<label for="name">Name:</label><br />
-				<input name="name" type="text" maxlength="128"  id="name"  size="50" required   /><br />
-				<label for="email">Email:</label><br />
-				<input name="email" type="email" maxlength="128"  id="email"  size="50" required /><br />
-				<label for="text">Message:</label><br />
-				<textarea name="text" rows="7" columns="50" cols="23" id="text" ></textarea><br />
-				<input name="Submit" type="submit" value="Submit" class="submit" id="submit" /><br />
-			</form>		
-	 	</div>
 
-	 	<div class="column full">
-			<h2>Site</h2>
-			<h3>Front End</h3>
-			<p>This is my fourth attempt at building a semantic HTML5 site.</p>
-			<p>I use <a href="https://typekit.com/">Typekit</a> to render <a href="https://typekit.com/fonts/museo-slab">Museo Slab</a> 
-			as a the header font and <a href="https://typekit.com/fonts/museo-sans">Museo Sans</a> for the copy font. </p>
-		 
-			
-			<h3>Tools</h3>
-			<ul>
-			<li>I used <a href="http://html.adobe.com/edge/inspect/">Edge Inspect</a> to get the site working cross device.</li>	
-			<li>The site was composed using <a href="http://www.adobe.com/products/photoshop/">Photoshop</a>.</li>
-			<li>The responsive part of the site was planned using <a href="http://html.adobe.com/edge/reflow/">Adobe Edge Reflow</a>.</li>
-			<li>The code was written using <a href="http://www.sublimetext.com/2">Sublime Text 2</a> 
-				and <a href="http://www.brackets.io">Brackets</a>.</li>
-			<li>I also used <a href="http://sass-lang.com/">Sass</a> to speed up my CSS work.</li>	
-		 	</ul>
-	 	</div>	
 	</article> 	
 </div>
 <?php include '../modules/page-footer.php';?>

5a3c6ca2f008b2078f526e88dfc216f654a07a29
Terrence Ryan <terry@terrenceryan.com>
2014-08-22 09:29:52 -0400

    Moving style to match resume.

--- modules/typekit.php
+++ modules/typekit.php
@@ -1,28 +1,9 @@
-    <script type="text/javascript">
-      (function() {
-        var config = {
-          kitId: 'rbx8rox',
-          scriptTimeout: 3000
-        };
-        var h = document.getElementsByTagName('html')[0];
-        h.className += ' wf-loading';
-        var t = setTimeout(function() {
-          h.className = h.className.replace(/(\s|^)wf-loading(\s|$)/g, ' ');
-          h.className += ' wf-inactive';
-        }, config.scriptTimeout);
-        var d = false;
-        var tk = document.createElement('script');
-        tk.src = '//use.typekit.net/' + config.kitId + '.js';
-        tk.type = 'text/javascript';
-        tk.async = 'true';
-        tk.onload = tk.onreadystatechange = function() {
-          var rs = this.readyState;
-          if (d || rs && rs != 'complete' && rs != 'loaded') return;
-          d = true;
-          clearTimeout(t);
-          try { Typekit.load(config); } catch (e) {}
-        };
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(tk, s);
-      })();
-    </script>
+<script type="text/javascript">
+  (function(d) {
+    var config = {
+      kitId: 'xbg0mwb',
+      scriptTimeout: 3000
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>

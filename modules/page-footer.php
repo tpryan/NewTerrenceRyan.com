23f8b78328b5d64a9b7c95610aed6516aeb27399
Terrence Ryan <terry@terrenceryan.com>
2014-08-22 16:48:04 -0400

    Tweaking content.

--- modules/page-footer.php
+++ modules/page-footer.php
@@ -1,5 +1,4 @@
 <footer>
 	<p class="copyright">Copyright 2008 - 2013 &nbsp;<strong>Terrence Ryan</strong></p>
-	<small class="disclaimer">This is a personal site. While Adobe paying my salary might 
-	not make me completely  objective, the opinions expressed here are mine and not theirs.</small>
+	<small class="disclaimer">This is a personal site &mdash; the opinions expressed here are mine and not my employer's.</small>
 </footer>

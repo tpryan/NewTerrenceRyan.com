d22816260f5fb8378afcb32cac5cdc69375e7339
Terrence Ryan <terry@terrenceryan.com>
2014-08-25 23:53:25 -0400

    Altered Resume content

--- resume/index.php
+++ resume/index.php
@@ -12,20 +12,226 @@
 	<h1>Resume</h1>
 	<article>
 		<div class="column full">
-			<img class="picture logo" src="/assets/img/adobelogo.png" id="adobelogo">
+			<h2>Objective</h2>
+			<p><em>I love technology that helps people extract ideas from their heads 
+				and into something real &mdashh; be it code, design, or hardware. I’m looking for a 
+				position that allows me use my people skills and technical knowledge to 
+				inspire people make their ideas concrete. </em></p>
+			<h2>Publications</h2>
 			
-			<p>I work as a Worldwide Developer Evangelist for Adobe Systems. My job basically entails me traveling the world and 
-			talking about the developer tools and technologies that Adobe has to offer or that we support.</p> 
+			<article class="position">
+				<header>
+					<h4>Pragmatic Bookshelf</h4>
+					<time>11/2010</time>
+				</header>
+				<div class="details">
+					<h3>Driving Technical Change</h3>
+					
+					<p>Details tools and techniques aiding developers to convince resistant co-workers 
+						to adopt new technologies. It presents hesitant coworkers’ reasoning as development 
+						anti-patterns.  It then presents solutions as positive patterns and outlines a 
+						broader strategy to run a comapaign to effect change inside one’s organization.</p>
+				</div>
+			</article>
+
+			<h2>Experience</h2>
+			<article class="position">
+				<header>
+					<h4>Adobe Systems</h4>
+					<time>11/2012 &mdash; Now</time>
+				</header>
+				<div class="details">
+					<h3>Group Manager Evangelism</h3>
+					<p>Head a small group dedicated to evangelizing Creative Cloud to educational 
+						institutions.  Role expanded to include a focus on enterprise customers.</p>
+					<p><strong>Activities Include:</strong></p>
+					<ul>
+						<li>Evangelize Creative Cloud using activities described in previous position</li>
+						<li>Oversee and guide the work of other evangelists</li>
+						<li>Coordinate enterprise marketing and greater evangelism team</li>
+					</ul>
+				</div>	
+			</article>	
+			<article class="position">
+				<header>
+					<h4>Adobe Systems</h4>
+					<time>09/2009 &mdash; 11/2012</time>
+				</header>
+				<div class="details">
+					<h3>Developer Evangelist</h3>
+					<p>Promoted use of Adobe tools and technology to developers with a focus on Higher 
+						Education. Technology focused on an evolving roster of client and server side web 
+						technologies including Flex, Flash, HTML5 and ColdFusion. The audience grew to include 
+						not just educators and developers but also web designers and traditional designers 
+						transitioning to the web.</p>
+					<p><strong>Activities Include:</strong></p>
+					<ul>
+						<li>Presenting to technical and creative audiences at events worldwide</li>
+						<li>Creating demo assets and projects</li>
+						<li>Speaking with the press </li>
+						<li>Producing blog posts, articles, and videos</li>
+						<li>Engaging with customers using social media</li>
+						<li>Writing code that shipped with Adobe ColdFusion</li>
+					</ul>
+				</div>		
+			</article>
+			<article class="position">
+				<header>
+					<h4>The Wharton School</h4>
+					<time>08/2008 &mdash; 01/2009</time>
+				</header>
+				<div class="details">
+					<h3>I.T. Director</h3>
+					<p>Modernized code base and publishing system for Knowledge@Wharton, an online journal, 
+						targeting audiences in English, Spanish, Portuguese, and Chinese speaking countries 
+						with millions of readers.</p>
+					<p><strong>Activities Include:</strong></p>
+					<ul>
+						<li>Upgraded custom journal site to new architecture using ColdFusion and Java</li>
+						<li>Consolidation of multi site publishing system into one code base</li>
+						<li>Performance boosts that reduced average page load time from 2000ms to 16ms</li>
+					</ul>
+				</div>	
+			</article>	
+			<article class="position">
+				<header>
+					<h4>The Wharton School</h4>
+					<time>6/2007 &mdash; 8/2008</time>
+				</header>
+				<div class="details">
+					<h3>I.T. Director</h3>
+					<p>Managed team to redesign and modernize WRDS web site/application for access to 
+						multi-terabyte warehouse of international financial data used by over 200 peer 
+						institution clients, providing revenue of about $9 million a year. </p>
+					<p><strong>Activities Include:</strong></p>
+					<ul>
+						<li>Converting existing site to Web Standards based code</li>
+						<li>Unifying search for over 200,000 academic texts</li>
+						<li>Creating a community driven site for the users of WRDS</li>
+						<li>Managing a staff to facilitate those goals</li>
+					</ul>
+				</div>	
+			</article>
+			<article class="position">
+				<header>
+					<h4>The Wharton School</h4>
+					<time>6/1999 &mdash; 6/2007</time>
+				</header>
+				<div class="details">
+					<h3>Various Roles</h3>
+					<p>Worked in several roles over an eight year period at Wharton, starting as an entry 
+						level help desk staffer in 1999, and ending as a manager for a group of three 
+						developers overseeing all web applications at the school by 2007.  The web 
+						applications group was responsible for  developer productivity through code reviews, 
+						deployment tools, custom open source frameworks and encouraging use of source control. </p>
+					<p><strong>Activities Include:</strong></p>
+					<ul>
+						<li>Supported a full service print shop</li>
+						<li>Oversaw the IT design of a new remote campus in San Francisco</li>
+						<li>Oversaw the IT design of a flagship state-of-the-art facility on home campus</li>
+						<li>Supported a staff of 30 lawyers/professors</li>
+
+					</ul>
+				</div>	
+			</article>			
+
+			<h2>Education</h2>
+			<article class="position">
+				<header>
+					<h4>The University of Pennsylvania</h4>
+					<time>9/1995 &mdash; 5/1999</time>
+				</header>
+				<div class="details">
+					<h3>Bachelor of Arts</h3>
+					<p>Psychology</p>
+				</div>	
+			</article>	
+			<h2>Skills</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Evangelism</th>
+						<th>Development</th>
+						<th>Web</th>
+						<th>Systems</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<ul>
+								<li>Public Speaking</li>
+								<li>Writing</li>
+								<li>Social Media</li>
+								<li>Video Production</li>
+							</ul>
+						</td>
+						<td>
+							<ul>
+								<li>git</li>
+								<li>Jenkins</li>
+								<li>Subversion</li>
+							</ul>
+						</td>
+						<td>
+							<ul>
+								<li>HTML/JS/CSS</li>
+								<li>PHP</li>
+								<li>ColdFusion</li>
+								<li>MySQL</li>
+							</ul>
+						</td>
+						<td>
+							<ul>
+								<li>Active Directory</li>
+								<li>IIS</li>
+								<li>Windows Load Balancing</li>
+							</ul>
+						</td>
+					</tr>
+				</tbody>	
+			</table>
 			
-			<p>Before we go any futher, I will make the following disclaimer:</p>
-			<div id="disclaimerpad">
-			<blockquote id="disclaimer"><p>This is a personal blog. While Adobe paying my salary 
-			might not make me completely objective, the opinions 
-			expressed here are mine and not theirs.</p> </blockquote>
-			</div>
+			<h2>Interests</h2>
+			<p>Code Generation; Semantic HTML; Webstandards; Web related technologies 
+				and languages; Converting people to new technologies; Mentoring developers; 
+				Process improvement; Automation; Cloud Services; Higher Education; </p>
+
+			<h2>Content</h2>
+			<ul id="socialnetworks">
+				<li>
+                    <a href="http://www.facebook.com/terry.ryan" title="Facebook | Terrence Ryan">
+                        <img src="/assets/img/facebookicon.png" height="55" width="116" alt="Facebook" />
+                    </a>
+                </li>
+            
+                <li>
+                    <a href="http://twitter.com/tpryan" title="Twitter / tpryan">
+                        <img src="/assets/img/twitter.png" height="55" width="155" alt="Twitter" class="twitter" />
+                    </a>
+                </li>
+                <li>
+                    <a href="http://twitter.com/tpryan" title="Lanyrd / tpryan">
+                        <img src="/assets/img/lanyrd.png" height="36" width="150" alt="Lanyrd" class="lanyrd" />
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.linkedin.com/in/terrencepryan" title="LinkedIn: Terrence Ryan">
+                    <img src="/assets/img/linkedin-icon.png" height="35" width="121" alt="LinkedIn" />
+                </a>	
+                </li>
+                <li>
+                    <a href="https://github.com/tpryan" title="github: Terrence Ryan">
+                    <img src="/assets/img/githublogo.png" height="45" width="100" alt="github" />
+                </a>	
+                </li>
+                <li>
+                    <a href="http://www.behance.net/tpryan" title="Behance: Terrence Ryan">
+                    <img src="/assets/img/behance.png" height="56" width="150" alt="behance" />
+                </a>	
+                </li>
 			
-			<p>My major goal is to help developers using Adobe technologies to be successful. Hopefully they'll be using 
-			Adobe tools to do that, but it's important to keep in mind that sometimes they can't.</p>
+			</ul>
 		</div>	
 </article>
 </div>

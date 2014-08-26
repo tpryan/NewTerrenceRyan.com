08cadd29865535cef7ff25a4ac2bc5effd85fc3f
Terrence Ryan <terry@terrenceryan.com>
2014-08-26 16:25:28 -0400

    I've been writing this things since 2004

--- modules/page-footer.php
+++ modules/page-footer.php
@@ -1,4 +1,4 @@
 <footer>
-	<p class="copyright">Copyright 2008 - 2013 &nbsp;<strong>Terrence Ryan</strong></p>
+	<p class="copyright">Copyright 2004 &mdash; 2014 &nbsp;<strong>Terrence Ryan</strong></p>
 	<small class="disclaimer">This is a personal site &mdash; the opinions expressed here are mine and not my employer's.</small>
 </footer>
